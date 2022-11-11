75414b1ea8228dcec8f01f023f275a77b0e238a8
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-11-11 11:14:10 -0300

    Se agregó UI framework

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -3,16 +3,19 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import { Provider as PaperProvider } from "react-native-paper";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
-		<NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-		</NavigationContainer>
+		<PaperProvider>
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen name="Home" component={HomeScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</PaperProvider>
 	);
 }
 
--- Frontend/screens/HomeScreen.tsx
+++ Frontend/screens/HomeScreen.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function Home() {
 	return (
 		<View style={styles.container}>
 			<Text>Home Screen</Text>
+			<Button
+				icon="camera"
+				mode="contained"
+				onPress={() => console.log("Pressed")}
+			>
+				Press me
+			</Button>
 			<StatusBar style="auto" />
 		</View>
 	);
